11c28dc748689d700775c6cfa16b70aea1e22ee2
Heleneomdahl <Heom0001@stud.kea.dk>
2025-01-30 12:10:43 +0100

    js tilføelser

--- script.js
+++ script.js
@@ -12,3 +12,43 @@ function menuClick() {
   burger.classList.remove("active");
   nav.classList.remove("active");
 }
+
+// data
+let productId = 1526;
+let productContainer = document.querySelector(".productContainer");
+
+fetch(`https://kea-alt-del.dk/t7/api/products/${productId}`)
+  .then((response) => response.json())
+  .then((data) => {
+    productContainer.innerHTML = `
+     <h1>Produkt</h1>
+        <p class="kategori_øverst">Hjem > Sport > Accessories > Tasker > Puma > Big Cat Backpack Black </p>
+
+        <div class="grid_1_1 box">
+            <div class="billede_produkt">
+                <img src="https://kea-alt-del.dk/t7/images/webp/640/${productId}.webp" alt="rygsæk">
+            </div>
+            <div>
+                <h2 class="productName">${data.productdisplayname}</h2>
+                <h3>Mærke:</h3>
+                <p class="tekst_produkt">${data.brandname}</p>
+                <h3>Pris:</h3>
+                <p class="tekst_produkt">${data.price}</p>
+                <h3>Kategori:</h3>
+                <p class="tekst_produkt">${data.category}</p>
+                <h3>Varenummer (id): </h3>
+                <p class="tekst_produkt">${data.id}</p>
+                <div class="læg_i_kurv">
+                    <a class="knap" href="produkt.html">Læg i indkøbskurv</a>
+                </div>
+
+
+
+
+
+            </div>
+
+
+        </div>
+    `;
+  });
