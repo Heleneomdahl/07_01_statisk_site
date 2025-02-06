2b78fd8cdc8daf740f1d066660685980b032fd5b
Heleneomdahl <Heom0001@stud.kea.dk>
2025-02-06 09:25:39 +0100

    liste virker

--- produktliste.js
+++ produktliste.js
@@ -4,7 +4,7 @@ const mycategory = urlParams.get("category");
 
 console.log("Kategori:", mycategory);
 
-let productContainer = document.querySelector("container");
+let productContainer = document.querySelector(".container");
 
 fetch(`https://kea-alt-del.dk/t7/api/products?category=${mycategory}`)
   // https://kea-alt-del.dk/t7/api/products/${productId} billeder
@@ -15,28 +15,10 @@ function showList(data) {
   console.log(data);
   const markup = data
     .map(
-      (element) =>
-        ` <p class="kategori_øverst"><a class="hjem_øverst" href="index.html">Hjem</a> > ${element.category}</p>
-        <div class="grid_1_1 box">
-            <div class="billede_produkt">
-                <img src="https://kea-alt-del.dk/t7/images/webp/640/${element.id}.webp" alt="rygsæk">
-            </div>
-            <div>
-                <h2 class="productName">${element.productdisplayname}</h2>
-                <h3>Mærke:</h3>
-                <p class="tekst_produkt">${element.brandname}</p>
-                <h3>Pris:</h3>
-                <p class="tekst_produkt">${element.price} DKK</p>
-                <h3>Kategori:</h3>
-                <p class="tekst_produkt">${element.category}</p>
-                <h3>Varenummer (id): </h3>
-                <p class="tekst_produkt">${element.id}</p>
-                <div class="læg_i_kurv">
-                    <a class="knap" href="produkt.html">Læg i indkøbskurv</a>
-                </div>
-            </div> `
+      (elem) => `
+  <article><h2>${elem.productdisplayname}</h2></article>
+  `
     )
-    .json("");
-  console.log(markup);
+    .join("");
   productContainer.innerHTML = markup;
 }
